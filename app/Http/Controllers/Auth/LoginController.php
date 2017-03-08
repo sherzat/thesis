d3c193aa1716e875dfc424f4e8ad9c6fb506d939
sherzat <sherzat.ablimit@gmail.com>
2017-03-08 10:29:19 +0100

    Reverting to the state of the project at 6fce590

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,6 +26,7 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = '/home';
+    protected $loginPath = '/';
 
     /**
      * Create a new controller instance.
